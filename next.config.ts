623d1ccca0d8b701bfc882ad3cdef3b9c6836b58
visualmulia <visualmulia@gmail.com>
2026-05-18 18:40:53 +0800

    chore(sentry): integrate withSentryConfig in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from 'next'
 import withPWA from '@ducanh2912/next-pwa'
+import { withSentryConfig } from '@sentry/nextjs'
 
 const nextConfig: NextConfig = {
   images: {
@@ -18,4 +19,13 @@ const withPWAConfig = withPWA({
   disable: process.env.NODE_ENV === 'development',
 })(nextConfig)
 
-export default withPWAConfig
+export default withSentryConfig(withPWAConfig, {
+  org: 'masbroo-studio',
+  project: 'logis',
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  tunnelRoute: '/monitoring',
+  hideSourceMaps: true,
+  disableLogger: true,
+  automaticVercelMonitors: true,
+})
